6ee28d75c66a41b9b0df4b14734e08d96bde409a
Soma Kundu <somakund09@gmail.com>
2026-08-03 13:17:19 +0530

    chore: update Vite development server port to 5176

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5176,
     host: "187.127.163.17",
     proxy: {
       "/api": {
